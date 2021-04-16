10073bb18e7462b03815339049123e9086631c32
= <=>
2021-04-16 10:56:48 +0530

    comitting Log4jdemo

---
+++ src/org/test/log4j2demo/Log4j2Demo.java
@@ -0,0 +1,14 @@
+package org.test.log4j2demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j2Demo {
+
+	private static Logger log = LogManager.getLogger();
+
+	public static void main(String[] args) {
+
+	}
+
+}
